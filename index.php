0739385c084223d6b75d4494f5f9161b845dd666
Hendzu <hendrikrasmus@hotmail.com>
2015-03-08 21:24:23 +0200

    Avaleht

--- index.php
+++ index.php
@@ -1,21 +1,13 @@
-  
-	<?php 
+<?php 
         include("header.html");         
-     ?>
+?>
 
-    <div id="main">
-    <h3>Get started with your website</h3>
+<!DOCTYPE html>
+<html>
+<body>
+  <h1>Default valimised</h1>
+  <h2>Vabandame!</h2>
+  <p>Antud leht ei ole hetkel valmis. Küsimuste tekkimiselt pöörduge meie arendustiimi liidri <a href=https://www.facebook.com/diana.algma>Diana</a> või kaasosalise <a href=https://www.facebook.com/hendrik.elmet>Hendriku</a> poole</p>
+</body>
 
-    <ol class="round">
-        <li class="one">
-            <h5>Login as admin </h5>
-           The site admin username and password are stored in the config file in the Includes directory. 
-        </li>
-        <li class="two">
-            <h5>Customize your site</h5>
-             After you login, you can add, delete, and modify web pages...
-         </li>
-    </ol>
-
-
-    </div>
+</html>
