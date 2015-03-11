57fa6b50f1c201a56fb5801c43e7b6f9d4348a8d
Hendzu <hendrikrasmus@hotmail.com>
2015-03-11 11:20:53 +0200

    õ

--- kandideeri.php
+++ kandideeri.php
@@ -4,7 +4,7 @@
     
   <h1>Default valimised</h1>
   <h2>Vabandame!</h2>
-  <p>Antud leht ei ole hetkel valmis. Küsimuste tekkimiselt pöörduge meie arendustiimi liidri <a href=https://www.facebook.com/diana.algma>Diana</a> v�i kaasosalise <a href=https://www.facebook.com/hendrik.elmet>Hendriku</a> poole</p>
+  <p>Antud leht ei ole hetkel valmis. Küsimuste tekkimiselt pöörduge meie arendustiimi liidri <a href=https://www.facebook.com/diana.algma>Diana</a> või kaasosalise <a href=https://www.facebook.com/hendrik.elmet>Hendriku</a> poole</p>
 </body>
 
 </html>
