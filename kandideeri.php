f6d81b18e7d21e36aa4e23bc9121658fc18df920
Hendzu <hendrikrasmus@hotmail.com>
2015-03-11 11:17:59 +0200

    Veel 1 viga

---
+++ kandideeri.php
@@ -0,0 +1,10 @@
+<?php 
+        include("header.php");         
+?>
+    
+  <h1>Default valimised</h1>
+  <h2>Vabandame!</h2>
+  <p>Antud leht ei ole hetkel valmis. Küsimuste tekkimiselt pöörduge meie arendustiimi liidri <a href=https://www.facebook.com/diana.algma>Diana</a> või kaasosalise <a href=https://www.facebook.com/hendrik.elmet>Hendriku</a> poole</p>
+</body>
+
+</html>
