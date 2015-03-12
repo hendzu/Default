087bdcb39922f2ac08d03e0f24fc79d4401fad70
dianx93 <dianx93@gmail.com>
2015-03-12 13:11:44 +0200

    proov 4, profiilipilt

--- header.php
+++ header.php
@@ -7,6 +7,16 @@
 	<body>
                 <div id="fb-root"></div>
                 <script>
+                    
+                  (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    var userInfo = document.getElementById('user-info');
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = "//connect.facebook.net/et_EE/sdk.js#xfbml=1&appId=1386129771704789&version=v2.0";
+                    fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));
+                    
                     function statusChangeCallback(response) {
                     console.log('statusChangeCallback');
                     console.log(response);
@@ -23,15 +33,12 @@
                       //  'into Facebook.';
                     }
                   }
-                  (function(d, s, id) {
-                    var js, fjs = d.getElementsByTagName(s)[0];
-                    var userInfo = document.getElementById('user-info');
-                    if (d.getElementById(id)) return;
-                    js = d.createElement(s); js.id = id;
-                    js.src = "//connect.facebook.net/et_EE/sdk.js#xfbml=1&appId=1386129771704789&version=v2.0";
-                    fjs.parentNode.insertBefore(js, fjs);
-                    }(document, 'script', 'facebook-jssdk'));
-                    
+                  function checkLoginState() {
+                        FB.getLoginStatus(function(response) {
+                          statusChangeCallback(response);
+                        });
+                      }
+                  
                     function testAPI() {
                         FB.api('/me', function(response) {
                          userInfo.innerHTML = '<img src="https://graph.facebook.com/' + response.id + '/picture">' +
@@ -47,5 +54,5 @@
 			<a href="http://defaultvalimised.azurewebsites.net/tulemused.php">Tulemused</a>
 		</p>
                 <div class="fb-login-button" data-max-rows="1" data-size="large" 
-                    data-show-faces="false" data-auto-logout-link="true"></div>
+                    data-show-faces="true" data-auto-logout-link="true"></div>
                 <div id="user-info"></div>
