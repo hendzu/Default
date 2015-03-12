8e850549b0ab4a3214339acd22b2e0e52f438ffc
dianx93 <dianx93@gmail.com>
2015-03-12 13:00:29 +0200

    proov 3, profiilipilt

--- header.php
+++ header.php
@@ -6,7 +6,24 @@
 	</head>
 	<body>
                 <div id="fb-root"></div>
-                <script>(function(d, s, id) {
+                <script>
+                    function statusChangeCallback(response) {
+                    console.log('statusChangeCallback');
+                    console.log(response);
+                    if (response.status === 'connected') {
+                      testAPI();
+                    //} else if (response.status === 'not_authorized') {
+                      // The person is logged into Facebook, but not your app.
+                      //document.getElementById('status').innerHTML = 'Please log ' +
+                      //  'into this app.';
+                    //} else {
+                      // The person is not logged into Facebook, so we're not sure if
+                      // they are logged into this app or not.
+                      //document.getElementById('status').innerHTML = 'Please log ' +
+                      //  'into Facebook.';
+                    }
+                  }
+                  (function(d, s, id) {
                     var js, fjs = d.getElementsByTagName(s)[0];
                     var userInfo = document.getElementById('user-info');
                     if (d.getElementById(id)) return;
@@ -14,13 +31,14 @@
                     js.src = "//connect.facebook.net/et_EE/sdk.js#xfbml=1&appId=1386129771704789&version=v2.0";
                     fjs.parentNode.insertBefore(js, fjs);
                     }(document, 'script', 'facebook-jssdk'));
-
-                    FB.api('/me', function(response) {
+                    
+                    function testAPI() {
+                        FB.api('/me', function(response) {
                          userInfo.innerHTML = '<img src="https://graph.facebook.com/' + response.id + '/picture">' +
-
                          response.name;
-
-                    });</script>
+                    });
+                      }
+                    </script>
 		<p>
 			<a href="http://defaultvalimised.azurewebsites.net/">Avaleht</a>
 			<a href="http://defaultvalimised.azurewebsites.net/haaleta.php">Hääleta</a>
