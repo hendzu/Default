3377a00661189a95a31145382dca35a4d119757b
dianx93 <dianx93@gmail.com>
2015-03-12 11:18:58 +0200

    test 4, html validation

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" prefix="fb: http://www.facebook.com/2008/fbml">
+<html lang="en" xmlns:fb="http://www.facebook.com/2008/fbml">
 	<head>
         <meta charset="utf-8" />
 		<title>Default Valimised</title>
