830f394823f2cab9236964f0ae2d40761fb4de84
dianx93 <dianx93@gmail.com>
2015-03-12 11:42:23 +0200

    nüüd eestikeelne login nupp

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
                     var js, fjs = d.getElementsByTagName(s)[0];
                     if (d.getElementById(id)) return;
                     js = d.createElement(s); js.id = id;
-                    js.src = "//connect.facebook.net/en_PI/sdk.js#xfbml=1&appId=1386129771704789&version=v2.0";
+                    js.src = "//connect.facebook.net/et_EE/sdk.js#xfbml=1&appId=1386129771704789&version=v2.0";
                     fjs.parentNode.insertBefore(js, fjs);
                     }(document, 'script', 'facebook-jssdk'));</script>
 		<p>
