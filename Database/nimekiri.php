b2ce72738583606ba8d402bcffd5cc5e8f5db1c3
Hendzu <hendrikrasmus@hotmail.com>
2015-03-18 23:55:30 +0200

    Ma ei tea mis toimub aga andmebaas peksab tõsiselt näkku..... isegi rohkem kui siis kui ta mul 15 minutit ühte tabelit tegi

--- Database/nimekiri.php
+++ Database/nimekiri.php
@@ -1,10 +1,11 @@
 <?php
 require_once("connect.php");
-    $sql ="Select ID, Nimi, Partei, Piirkond From kandidaadid";
+    $sql ="Select kandidaadid.ID, kandidaadid.Nimi, parteid.nimi, kandidaadid.Piirkond
+From kandidaadid join parteid on kandidaadid.partei_id=parteid.id;";
     echo "<table>";
     echo "<tr><th>ID</th><th>Nimi</th><th>Partei</th><th>Piirkond</th></tr>";
     foreach ($conn->query($sql) as $row) {
-        echo "<tr><td>".$row['ID']."</td><td>".$row['Nimi']."</td><td>".$row['Partei']."</td><td>".$row['Piirkond']."</td></tr>";
+        echo "<tr><td>".$row['kandidaadid.ID']."</td><td>".$row['kandidaadid.Nimi']."</td><td>".$row['parteid.nimi']."</td><td>".$row['kandidaadid.Piirkond']."</td></tr>";
     }
 	
     echo "</table>";
