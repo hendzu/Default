96ebd751a53d160d740142d1e2a014abd61e47ec
dianx93 <dianx93@gmail.com>
2015-03-18 10:41:17 +0200

    üritan login nuppu tagasi saada..

--- header.php
+++ header.php
@@ -40,12 +40,10 @@
                       }
                   
                     function testAPI() {
-                        console.log('Welcome!  Fetching your information.... ');
                         FB.api('/me', function(response) {
-                          console.log('Successful login for: ' + response.name);
-                          document.getElementById('status').innerHTML =
-                            'Thanks for logging in, ' + response.name + '!';
-                        });
+                         userInfo.innerHTML = '<img src="https://graph.facebook.com/' + response.id + '/picture">' +
+                         response.name;
+                    });
                       }
                     </script>
 		<p>
