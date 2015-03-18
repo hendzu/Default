55013cdb1ec74362e8b2d06b086744c1828505c0
Hendzu <hendrikrasmus@hotmail.com>
2015-03-18 22:57:03 +0200

    Tegin nata ilusamaks

--- Database/connect.php
+++ Database/connect.php
@@ -1,31 +1,13 @@
 <?php
-/*Server: cikj0w7mbn.database.windows.net,1433
-SQL Database: defaultAndmebaas
-User Name: defaultkasutaja
-
-PHP Data Objects(PDO) Sample Code:
-<h2>Vabandame!</h2>*/
 
 try {
-	$conn = new PDO ( "sqlsrv:server = tcp:cikj0w7mbn.database.windows.net,1433; Database = defaultAndmebaas", "defaultkasutaja", "Isherenow0");
+	$conn = new PDO ( "sqlsrv:server = tcp:cikj0w7mbn.database.windows.net,1433; Database = defaultAndmebaas", "defaultkasutaja", "Isherenow0", array(
+    PDO::ATTR_PERSISTENT => true
+));
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	}
     	catch ( PDOException $e ) {
 		print( "Error connecting to SQL Server." );
 		die(print_r($e));
-		}
-    echo "<p>Kala</p><p>Kala2</p>";    
-    $sql ="Select ID, Nimi, Partei, Piirkond From kandidaadid";
-    echo "<table>";
-    echo "<tr><th>ID</th><th>Nimi</th><th>Partei</th><th>Piirkond</th></tr>";
-    foreach ($conn->query($sql) as $row) {
-        //echo "<tr><td>$row</td><td>Nimi</td><td>Partei</td><td>Piirkond</td></tr>";
-        echo "<tr><td>".$row['ID']."</td><td>".$row['Nimi']."</td><td>".$row['Partei']."</td><td>".$row['Piirkond']."</td></tr>";
-    }
-    echo "</table>";
-		/*rSQL Server Extension Sample Code:
-		
-		$connectionInfo = array("UID" => "defaultkasutaja@cikj0w7mbn", "pwd" => "Isherenow0", "Database" => "defaultAndmebaas", "LoginTimeout" => 30, "Encrypt" => 1);
-        $serverName = "tcp:cikj0w7mbn.database.windows.net,1433";
-		$conn = sqlsrv_connect($serverName, $connectionInfo);*/
+		} 
 ?>
--- kandidaadid.php
+++ kandidaadid.php
@@ -5,7 +5,7 @@
   <h1>Default valimised</h1>
   <h2>Vabandame!</h2>
     <?php 
-        include("Database/connect.php");       
+        include("Database/nimekiri.php");       
 ?>
   <p>Antud leht ei ole hetkel valmis. Küsimuste tekkimiselt pöörduge meie arendustiimi liidri <a href=https://www.facebook.com/diana.algma>Diana</a> või kaasosalise <a href=https://www.facebook.com/hendrik.elmet>Hendriku</a> poole</p>
 </body>
