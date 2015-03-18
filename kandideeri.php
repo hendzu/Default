553a5fc63ac0650cfc17743e824e48fd898ee459
dianx93 <dianx93@gmail.com>
2015-03-18 11:26:58 +0200

    aga nüüd...?

--- kandideeri.php
+++ kandideeri.php
@@ -1,7 +1,7 @@
 <?php 
         include("header.php");         
-        $helper = new FacebookRedirectLoginHelper('your redirect URL here');
-        $loginUrl = $helper->getLoginUrl();
+        //$helper = new FacebookRedirectLoginHelper('your redirect URL here');
+        //$loginUrl = $helper->getLoginUrl();
         // Use the login url on a link or button to redirect to Facebook for authentication
 
         $helper = new FacebookRedirectLoginHelper();
@@ -21,9 +21,9 @@
       echo htmlentities($html);
 ?>
     
-<h1>Default valimised</h1>
-  <h2>Vabandame!</h2>
-  <p>Antud leht ei ole hetkel valmis. Küsimuste tekkimiselt pöörduge meie arendustiimi liidri <a href=https://www.facebook.com/diana.algma>Diana</a> või kaasosalise <a href=https://www.facebook.com/hendrik.elmet>Hendriku</a> poole</p>
+    <h1>Default valimised</h1>
+    <h2>Vabandame!</h2>
+    <p>Antud leht ei ole hetkel valmis. Küsimuste tekkimiselt pöörduge meie arendustiimi liidri <a href=https://www.facebook.com/diana.algma>Diana</a> või kaasosalise <a href=https://www.facebook.com/hendrik.elmet>Hendriku</a> poole</p>
 </body>
 
 </html>
