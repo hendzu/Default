e7242b921edd4eae3de09671c2586a7b09dcabde
Hendzu <hendrikrasmus@hotmail.com>
2015-03-18 20:19:39 +0200

    It fucking works

--- Database/connect.php
+++ Database/connect.php
@@ -14,14 +14,15 @@ try {
 		print( "Error connecting to SQL Server." );
 		die(print_r($e));
 		}
-        
-    $sql ="Select Nimi, Partei, Piirkond From kandidaadid";
+    echo "<p>Kala</p><p>Kala2</p>";    
+    $sql ="Select ID, Nimi, Partei, Piirkond From kandidaadid";
+    echo "<table>";
+    echo "<tr><th>ID</th><th>Nimi</th><th>Partei</th><th>Piirkond</th></tr>";
     foreach ($conn->query($sql) as $row) {
-        print $row['Nimi'] . "\t";
-        print $row['Partei'] . "\t";
-        print $row['Piirkond'] . "\n";
+        //echo "<tr><td>$row</td><td>Nimi</td><td>Partei</td><td>Piirkond</td></tr>";
+        echo "<tr><td>".$row['ID']."</td><td>".$row['Nimi']."</td><td>".$row['Partei']."</td><td>".$row['Piirkond']."</td></tr>";
     }
-
+    echo "</table>";
 		/*rSQL Server Extension Sample Code:
 		
 		$connectionInfo = array("UID" => "defaultkasutaja@cikj0w7mbn", "pwd" => "Isherenow0", "Database" => "defaultAndmebaas", "LoginTimeout" => 30, "Encrypt" => 1);
