76dd3a15882a01638f5761279cebb2eb9b297287
Hendzu <hendrikrasmus@hotmail.com>
2015-03-18 19:13:26 +0200

    EElmine oli vale

--- Database/connect.php
+++ Database/connect.php
@@ -1,19 +1,30 @@
 <?php
-Server: cikj0w7mbn.database.windows.net,1433
+/*Server: cikj0w7mbn.database.windows.net,1433
 SQL Database: defaultAndmebaas
 User Name: defaultkasutaja
 
 PHP Data Objects(PDO) Sample Code:
+<h2>Vabandame!</h2>*/
+
 try {
-	$conn = new PDO ( \"sqlsrv:server = tcp:cikj0w7mbn.database.windows.net,1433; Database = defaultAndmebaas\", \"defaultkasutaja\", \"{your_password_here}\");
+	$conn = new PDO ( "sqlsrv:server = tcp:cikj0w7mbn.database.windows.net,1433; Database = defaultAndmebaas", "defaultkasutaja", "Isherenow0");
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	}
-	catch ( PDOException $e ) {
-		print( \"Error connecting to SQL Server.\" );
+    	catch ( PDOException $e ) {
+		print( "Error connecting to SQL Server." );
 		die(print_r($e));
 		}
-		rSQL Server Extension Sample Code:
+        
+    $sql ="Select Nimi, Partei, Piirkond From kandidaadid";
+    foreach ($conn->query($sql) as $row) {
+        print $row['Nimi'] . "\t";
+        print $row['Partei'] . "\t";
+        print $row['Piirkond'] . "\n";
+    }
+
+		/*rSQL Server Extension Sample Code:
 		
-		$connectionInfo = array(\"UID\" => \"defaultkasutaja@cikj0w7mbn\", \"pwd\" => \"{your_password_here}\", \"Database\" => \"defaultAndmebaas\", \"LoginTimeout\" => 30, \"Encrypt\" => 1);\r\n$serverName = \"tcp:cikj0w7mbn.database.windows.net,1433\";
-		$conn = sqlsrv_connect($serverName, $connectionInfo);
+		$connectionInfo = array("UID" => "defaultkasutaja@cikj0w7mbn", "pwd" => "Isherenow0", "Database" => "defaultAndmebaas", "LoginTimeout" => 30, "Encrypt" => 1);
+        $serverName = "tcp:cikj0w7mbn.database.windows.net,1433";
+		$conn = sqlsrv_connect($serverName, $connectionInfo);*/
 ?>
