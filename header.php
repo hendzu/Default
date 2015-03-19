2eff29af1840bca32ee6fb6d7e99ccbd7aec37cc
dianx93 <dianx93@gmail.com>
2015-03-19 12:23:03 +0200

    eestikeelseks

--- header.php
+++ header.php
@@ -71,18 +71,18 @@
                       var js, fjs = d.getElementsByTagName(s)[0];
                       if (d.getElementById(id)) return;
                       js = d.createElement(s); js.id = id;
-                      js.src = "//connect.facebook.net/en_US/sdk.js";
+                      js.src = "//connect.facebook.net/et_EE/sdk.js";
                       fjs.parentNode.insertBefore(js, fjs);
                     }(document, 'script', 'facebook-jssdk'));
 
                     // Here we run a very simple test of the Graph API after login is
                     // successful.  See statusChangeCallback() for when this call is made.
                     function testAPI() {
-                      console.log('Welcome!  Fetching your information.... ');
+                      console.log('Tere tulemast!  Ootame sinu andmeid.... ');
                       FB.api('/me', function(response) {
-                        console.log('Successful login for: ' + response.name);
+                        console.log('Edukas sisselogimine: ' + response.name);
                         document.getElementById('status').innerHTML =
-                          'Thanks for logging in, ' + response.name + '!';
+                          'Olete sisse logitud, ' + response.name + '!';
                       });
                     }
                   </script>
