949c0f86e65078851395871506f71d9c52ceaa6c
dianx93 <dianx93@gmail.com>
2015-03-20 15:22:01 +0200

    navBari styling olemas nüüd

--- header.php
+++ header.php
@@ -4,26 +4,28 @@
         <meta charset="utf-8" />
 		<title>Default Valimised</title>
                 <script type="text/javascript" src="js/fblogin.js"></script>
-	</head>
+                <link rel="stylesheet" type="text/css" href="Styles/mySite.css">
+        </head>
 	<body>
                 <div id="fb-root"></div>
-                
-
-		<p>
-			<a href="http://defaultvalimised.azurewebsites.net/">Avaleht</a>
-			<a href="http://defaultvalimised.azurewebsites.net/haaleta.php">Hääleta</a>
-			<a href="http://defaultvalimised.azurewebsites.net/kandidaadid.php">Kandidaadid</a>
-			<a href="http://defaultvalimised.azurewebsites.net/kandideeri.php">Kandideeri</a>
-			<a href="http://defaultvalimised.azurewebsites.net/tulemused.php">Tulemused</a>
-		</p>
-                
+                <div id="navBar">
+                    <ul>
+                        <li><a href="http://defaultvalimised.azurewebsites.net/">Avaleht</a></li>
+                        <li><a href="http://defaultvalimised.azurewebsites.net/haaleta.php">Hääleta</a></li>
+                        <li><a href="http://defaultvalimised.azurewebsites.net/kandidaadid.php">Kandidaadid</a></li>
+                        <li><a href="http://defaultvalimised.azurewebsites.net/kandideeri.php">Kandideeri</a></li>
+                        <li><a href="http://defaultvalimised.azurewebsites.net/tulemused.php">Tulemused</a></li>
+                        <li><div class="fb-login-button" data-max-rows="1" data-size="large"
+                     data-show-faces="false" data-auto-logout-link="true"
+                     data-onlogin="checkLoginStatus();"></div></li>
+                    </ul>
+                </div>
+		
                 <!--
                     Below we include the Login Button social plugin. This button uses
                     the JavaScript SDK to present a graphical Login button that triggers
                     the FB.login() function when clicked.
                   -->
-                <div class="fb-login-button" data-max-rows="1" data-size="large"
-                     data-show-faces="false" data-auto-logout-link="true"
-                     data-onlogin="checkLoginStatus();"></div>
+                
                 <div id="status">
                 </div>
