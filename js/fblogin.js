15450fd8c50f8af23555b78a609d80e2616f96b5
dianx93 <dianx93@gmail.com>
2015-03-22 14:33:38 +0200

    ei saanud kandideerimist tööle, reverse'in

--- js/fblogin.js
+++ js/fblogin.js
@@ -7,7 +7,7 @@
                       // Full docs on the response object can be found in the documentation
                       // for FB.getLoginStatus().
                       if (response.status === 'connected') {
-                        //LogitudKandideerima();
+                        //Logged into Facebook;
                         testAPI();
                       } else if (response.status === 'not_authorized') {
                         // The person is logged into Facebook, but not your app.
