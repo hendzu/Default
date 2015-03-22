bb69875c6c082adfe4ea20ac70c83ede250b2e65
Hendzu <hendrikrasmus@hotmail.com>
2015-03-22 19:49:54 +0200

    Üleliigsed asjad

--- Database/kandideerimisvorm.php
+++ Database/kandideerimisvorm.php
@@ -5,14 +5,14 @@ require("connect.php");
     $sql ="Select ID, Nimi From parteid;";
     echo "<h3>Vali partei</h3>";
     foreach ($conn->query($sql) as $row) {
-        echo '<input type="radio" name="partei" value='.$row[0].'checked>'.$row[1].'<br>';
+        echo '<input type="radio" name="partei" value='.$row[0].'>'.$row[1].'<br>';
     }
 	
 	
     echo "<h3>Vali piirkond</h3>";
 	$sql ="Select ID,Piirkond From piirkonnad;";
     foreach ($conn->query($sql) as $row) {
-        echo '<input type="radio" name="piirkond" value='.$row[0].'checked>'.$row[1].'<br>';
+        echo '<input type="radio" name="piirkond" value='.$row[0].'>'.$row[1].'<br>';
     }
 	
     echo '<input type="button" onclick="'."alert('Tere')".'" value="Kinnita"></form>';
