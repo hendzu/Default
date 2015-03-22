6a160fc33dd68965ecc1b47abbe93de7a71e9b25
Hendzu <hendrikrasmus@hotmail.com>
2015-03-22 19:43:10 +0200

    Tegin mingi mitte funkava vormi valmis

---
+++ Database/kandideerimisvorm.php
@@ -0,0 +1,20 @@
+<?php
+require("connect.php");
+
+    echo "<form>";
+    $sql ="Select ID, Nimi From parteid;";
+    echo "<h3>Vali partei</h3>";
+    foreach ($conn->query($sql) as $row) {
+        echo '<input type="radio" name="partei" value='.$row[0].'checked>'.$row[1].'<br>';
+    }
+	
+	
+    echo "<h3>Vali piirkond</h3>";
+	$sql ="Select ID,Piirkond From piirkonnad;";
+    foreach ($conn->query($sql) as $row) {
+        echo '<input type="radio" name="piirkond" value='.$row[0].'checked>'.$row[1].'<br>';
+    }
+	
+    echo '<input type="button" onclick="alert("Tere")" value="Kinnita"></form>';
+require("disconnect.php");
+?>
--- kandideeri.php
+++ kandideeri.php
@@ -21,10 +21,9 @@
       //echo htmlentities($html);
 ?>
     
-    <h1>Default valimised</h1>
-    <h2>Vabandame!</h2>
-    <p>Antud leht ei ole hetkel valmis. Küsimuste tekkimiselt pöörduge meie arendustiimi liidri <a href=https://www.facebook.com/diana.algma>Diana</a> või kaasosalise <a href=https://www.facebook.com/hendrik.elmet>Hendriku</a> poole</p>
-    
+    <?php 
+        include("Database/kandideerimisvorm.php");       
+?>
 
 </body>
 
