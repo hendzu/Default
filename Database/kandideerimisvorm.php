5f2d96f448e73539dd893c18c5423163bcb57f54
dianx93 <dianx93@gmail.com>
2015-04-01 10:51:37 +0300

    #26

--- Database/kandideerimisvorm.php
+++ Database/kandideerimisvorm.php
@@ -1,11 +1,38 @@
 <?php
+
 require("connect.php");
-    echo '<script type="text/javascript" src="js/fblogin.js"></script>';
-    echo 'response.status';
-    if (response.status == 'connected'){
+    /*echo '<script type="text/javascript" src="js/fblogin.js"></script>';
+    //scriptist on vaja saada response.status ja response.name, siis edasine
+    //peaks töötama
+    echo 'response.status';*/
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
+
+if($session) {
+
+  try {
+
+    $user_profile = (new FacebookRequest(
+      $session, 'GET', '/me'
+    ))->execute()->getGraphObject(GraphUser::className());
+
+    echo $name=$user_profile->getName();
+    /*
+    echo "Name: " . $user_profile->getName();
+*/
+  } catch(FacebookRequestException $e) {
+
+    echo "Exception occured, code: " . $e->getCode();
+    echo " with message: " . $e->getMessage();
+
+  }   
+
+}
+if ($session){
     echo '<form action="kinnitus.php" method="get">';
         echo '<h3>Nimi</h3>
-<input type="text" name="nimi" value=response.name readonly>';
+<input type="text" name="nimi" value=$name readonly>';
     $sql ="Select ID, Nimi From parteid;";
     echo "<h3>Vali partei</h3>";
     foreach ($conn->query($sql) as $row) {
