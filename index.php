d8cec0b264f6e24641e2c5c18bd85c85b8a1de20
Hendzu <hendrikrasmus@hotmail.com>
2015-04-01 15:31:37 +0300

    #24 Tegin kõik ühe lehe peal töötavaks. Ja hashid on ka aga nendel puuduv veel funktsionaalsus.

--- index.php
+++ index.php
@@ -1,10 +1,36 @@
-<?php 
-        include("header.php");
-?>
-    
-  <h1>Default valimised</h1>
-  <h2>Vabandame!</h2>
-  <p>Antud leht ei ole hetkel valmis. Küsimuste tekkimiselt pöörduge meie arendustiimi liidri <a href=https://www.facebook.com/diana.algma>Diana</a> või kaasosalise <a href=https://www.facebook.com/hendrik.elmet>Hendriku</a> poole</p>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+        <meta charset="utf-8" />
+		<title>Default Valimised</title>
+                <script type="text/javascript" src="js/fblogin.js"></script>
+				<script type="text/javascript" src="js/ajax.js"></script>
+                <link rel="stylesheet" type="text/css" href="Styles/mySite.css">
+        </head>
+	<body>
+                <div id="fb-root"></div>
+                <div id="navBar">
+                    <ul>
+                        <li><button type="button" onclick="lehevahetus('Database/nimekiri')">Avaleht</button></li>
+                        <li><button type="button" onclick="lehevahetus('Database/nimekiri')">Hääleta</button></li>
+                        <li><button type="button" onclick="lehevahetus('Database/nimekiri')">Kandidaadid</button></li>
+                        <li><button type="button" onclick="lehevahetus('Database/kandideerimisvorm')">Kandideeri</button></li>
+                        <li><button type="button" onclick="lehevahetus('Database/stat')">Statistika</button></li>
+                        <li><div class="fb-login-button" data-max-rows="1" data-size="large"
+                     data-show-faces="false" data-auto-logout-link="true"></div></li>
+                    </ul>
+                </div>
+		
+                <!--
+                    Below we include the Login Button social plugin. This button uses
+                    the JavaScript SDK to present a graphical Login button that triggers
+                    the FB.login() function when clicked.
+                  -->
+                
+                <div id="status">
+                </div>
+    <div id="sisu">
+    </div>
+ </body>
 
 </html>
--- js/ajax.js
+++ js/ajax.js
@@ -19,3 +19,7 @@ xmlhttp.onreadystatechange=function()
 xmlhttp.open("GET",file,true);
 xmlhttp.send();
 }
+function lehevahetus(leht){
+ajax('sisu',leht+'.php');
+window.location.hash=leht;
+}
