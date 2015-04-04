c900cfcd81ab143e4f50741e2ed4b35a533f533a
Hendzu <hendrikrasmus@hotmail.com>
2015-04-04 22:24:32 +0300

    And I found out why I couldn't commit changes

--- Database/kandideerimisvorm.php
+++ Database/kandideerimisvorm.php
@@ -1,29 +1,25 @@
 <?php
 
 require("connect.php");
-    //scriptist on vaja saada response.status ja response.name, siis edasine
-    //peaks töötama
-    //echo 'response.status';
-    echo '<form>';
+$fail="'kinnitus.php'";
+    echo '<form name="ajaxform" id="ajaxform" action="#kinnitus" method="post">';
+    echo '<input type="text" id="n" name="nimi" value="" readonly hidden>';
     $sql ="Select ID, Nimi From parteid;";
     echo "<h3>Vali partei</h3>";
     foreach ($conn->query($sql) as $row) {
-        echo '<input type="radio" name="partei" value='.$row[0].'>'.$row[1].'<br>';
+        echo '<input type="radio" id="p1" name="partei" value='.$row[0].'>'.$row[1].'<br>';
     }
 	
 	
     echo "<h3>Vali piirkond</h3>";
 	$sql ="Select ID,Piirkond From piirkonnad;";
     foreach ($conn->query($sql) as $row) {
-        echo '<input type="radio" name="piirkond" value='.$row[0].'>'.$row[1].'<br>';
+        echo '<input type="radio" id="p2" name="piirkond" value='.$row[0].'>'.$row[1].'<br>';
     }
-	
+    $div='confirm';
+    $leht='kinnitus';
+    echo '<input type="submit" name="submit" value="Kinnita" />';
     echo '</form>';
-    
-    echo '<input type="submit" onclick=kontroll() value="Kinnita"/>';
-    /*}
-    else {
-        echo '<p id="logisisse">Kandideerimiseks peate olema sisse logitud.</p>';
-    }*/
+    echo"<div class='confirm'></div>";
 require("disconnect.php");
 ?>
--- kinnitus.php
+++ kinnitus.php
@@ -1,11 +1,15 @@
+  <h2>Teie avaldus ei ole esitatud</h2>
 <?php 
-        include("header.php");
-        require("Database/saadaavaldus.php");
-		kandideeri($_GET["nimi"],$_GET["partei"],$_GET["piirkond"],$conn);
-		require("Database/disconnect.php");
+require("Database/connect.php");
+echo $_SERVER['REQUEST_METHOD'];
+if($_SERVER['REQUEST_METHOD'] == "GET"){ 
+    $sql ="Insert into kandidaadid (Nimi, Partei_id,Piirkond_id) values ('Kadri',".$_POST['partei'].",".$_POST['piirkond'].");";
+    echo $_REQUEST['partei'];
+    echo "Kala";
+    //$conn->query($sql);
+
+   // kandideeri($_POST['nimi'],$_POST['partei'],$_POST['piirkond'],$conn);
+
+require("Database/disconnect.php");}
 ?>
-
   <h2>Teie avaldus on esitatud</h2>
-</body>
-
-</html>
