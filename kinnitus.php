39c66aa788acf0d6aae605bd5467935d1a67ca15
Hendzu <hendrikrasmus@hotmail.com>
2015-04-05 15:43:31 +0300

    Viskas üleliigset infot välja.

--- kinnitus.php
+++ kinnitus.php
@@ -1,10 +1,8 @@
 <?php 
 require("Database/connect.php");
-echo $_SERVER['REQUEST_METHOD'];
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){ 
     $sql ="Insert into kandidaadid (Nimi, Partei_id,Piirkond_id) values ('".$_POST['nimi']."',".$_POST['partei'].",".$_POST['piirkond'].");";
-    echo $_REQUEST['partei'];
     $conn->query($sql);
 
 
