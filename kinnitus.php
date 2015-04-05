b3822d36d8df239d5f58d7b6019f3ddbcaf5df97
Hendzu <hendrikrasmus@hotmail.com>
2015-04-05 14:53:52 +0300

    Lets see if I commit it will it fix it

--- kinnitus.php
+++ kinnitus.php
@@ -2,13 +2,14 @@
 <?php 
 require("Database/connect.php");
 echo $_SERVER['REQUEST_METHOD'];
-if($_SERVER['REQUEST_METHOD'] == "GET"){ 
+
+    echo "Kalamari";
+if($_SERVER['REQUEST_METHOD'] == "POST"){ 
     $sql ="Insert into kandidaadid (Nimi, Partei_id,Piirkond_id) values ('Kadri',".$_POST['partei'].",".$_POST['piirkond'].");";
     echo $_REQUEST['partei'];
     echo "Kala";
-    //$conn->query($sql);
+    $conn->query($sql);
 
-   // kandideeri($_POST['nimi'],$_POST['partei'],$_POST['piirkond'],$conn);
 
 require("Database/disconnect.php");}
 ?>
