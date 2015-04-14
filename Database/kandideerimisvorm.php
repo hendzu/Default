2983d96fded81809720f0ec1ec1066e42ad1f3ce
dianx93 <dianx93@gmail.com>
2015-04-14 23:55:38 +0300

    tahan teada, milline ta nüüd on

--- Database/kandideerimisvorm.php
+++ Database/kandideerimisvorm.php
@@ -8,14 +8,14 @@ echo '<div id="vorm">';
     $sql ="Select ID, Nimi From parteid;";
     echo "<h3>Vali partei</h3>";
     foreach ($conn->query($sql) as $row) {
-        echo '<input type="radio" id="p1" name="partei" value='.$row[0].'>'.$row[1].'<br>';
+        echo '<input type="radio" id="p1" class="css-checkbox" name="partei" value='.$row[0].'>'.$row[1].'<br>';
     }
 	
 	
     echo "<h3>Vali piirkond</h3>";
 	$sql ="Select ID,Piirkond From piirkonnad;";
     foreach ($conn->query($sql) as $row) {
-        echo '<input type="radio" id="p2" name="piirkond" value='.$row[0].'>'.$row[1].'<br>';
+        echo '<input type="radio" id="p2" class="css-checkbox" name="piirkond" value='.$row[0].'>'.$row[1].'<br>';
     }
     echo '<input type="submit" name="submit" value="Kinnita" />';
     
