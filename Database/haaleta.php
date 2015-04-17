876fb91c8ed46e58919404770a0e7bc581ddc9d0
Hendzu <hendrikrasmus@hotmail.com>
2015-04-17 23:22:12 +0300

    Long overdue stuff

--- Database/haaleta.php
+++ Database/haaleta.php
@@ -8,9 +8,12 @@ $fail="'Database/haal.php'";
     $sql ="Select kandidaadid.ID, kandidaadid.Nimi, parteid.Nimi, piirkonnad.Piirkond
 From kandidaadid join parteid on kandidaadid.partei_id=parteid.id join piirkonnad on kandidaadid.piirkond_id=piirkonnad.id ;";
     echo "<h3>Vali partei</h3>";
+    echo "<table>";
+    echo "<tr><th></th><th>ID</th><th>Nimi</th><th>Partei</th><th>Piirkond</th></tr>";
     foreach ($conn->query($sql) as $row) {
-        echo '<input type="radio" id="p1" name="kandidaat" value='.$row[0].'>ID: '.$row[0].' Nimi: '.$row[1].' Partei: '.$row[2].' Piirkond: '.$row[3].'<br>';
+        echo '<tr><td><input type="radio" id="p1" name="kandidaat" value='.$row[0].'></td><td>'.$row[0].'</td><td>'.$row[1].'</td><td>'.$row[2].'</td><td>'.$row[3].'</td></tr>';
     }
+    echo "</table>";
     echo '<input type="submit" name="submit" value="Kinnita" />';
     
     echo"<div class='confirm'></div>";
