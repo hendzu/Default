655ca071e5012eccb97d48b2fcb3316d32a8764a
dianx93 <dianx93@gmail.com>
2015-05-19 18:22:56 +0300

    Nimekirjale ka pealkiri

--- Database/nimekiri.php
+++ Database/nimekiri.php
@@ -3,6 +3,7 @@ require("connect.php");
     $sql ="Select kandidaadid.ID, kandidaadid.Nimi, parteid.Nimi, piirkonnad.Piirkond
 From kandidaadid join parteid on kandidaadid.partei_id=parteid.id join piirkonnad on kandidaadid.piirkond_id=piirkonnad.id ;";
     echo '<meta charset="utf-8" />';
+    echo "<h3>Kandidaatide nimekiri</h3>";
     echo "<table>";
     echo "<tr><th>ID</th><th>Nimi</th><th>Partei</th><th>Piirkond</th></tr>";
     foreach ($conn->query($sql) as $row) {
