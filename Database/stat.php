01c482befb80c68864ac46d0cb37e1c29533c1db
dianx93 <dianx93@gmail.com>
2015-05-20 12:18:03 +0300

    ok, las see rownr olla praegu

--- Database/stat.php
+++ Database/stat.php
@@ -36,7 +36,7 @@ group by kandidaadid.nimi;";
     ?>
 <h3>Häälte jagunemine kogu riigis</h3>
 <?php
-    $sql ="Select row_number(), kandidaadid.Nimi, parteid.Nimi, piirkonnad.Piirkond, count(*)
+    $sql ="Select kandidaadid.Nimi, parteid.Nimi, piirkonnad.Piirkond, count(*)
     From kandidaadid join parteid on kandidaadid.partei_id=parteid.id 
     join piirkonnad on kandidaadid.piirkond_id=piirkonnad.id 
     join haaled on haaled.kandidaat_id=kandidaadid.id
@@ -44,9 +44,9 @@ group by kandidaadid.nimi;";
     ORDER BY count(*) DESC;";
     
     echo "<table>";
-    echo "<tr><th>Koht</th><th>Kandidaat</th><th>Partei</th><th>Piirkond</th><th>Hääled</th></tr>";
+    echo "<tr><th>Kandidaat</th><th>Partei</th><th>Piirkond</th><th>Hääled</th></tr>";
     foreach ($conn->query($sql) as $row) {
-        echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td><td>".$row[4]."</td></tr>";
+        echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td></tr>";
     }
     echo "</table>";
 ?>
