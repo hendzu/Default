05e33f9701187191a988b5e07db0e12ce251c5a6
dianx93 <dianx93@gmail.com>
2015-05-20 11:22:01 +0300

    äkki nüüd

--- Database/stat.php
+++ Database/stat.php
@@ -36,14 +36,14 @@ group by kandidaadid.nimi;";
     ?>
 <h3>Häälte jagunemine kogu riigis</h3>
 <?php
-    $sql ="Select kandidaadid.Nimi,count(*),parteid.Nimi 
+    $sql ="Select kandidaadid.Nimi,parteid.Nimi,count(*) 
         From haaled join kandidaadid on haaled.kandidaat_id=kandidaadid.id 
         join parteid on parteid.id=kandidaadid.partei_id 
         group by kandidaadid.nimi;";
     echo "<table>";
     echo "<tr><th>Kandidaat</th><th>Partei</th><th>Hääli</th></tr>";
     foreach ($conn->query($sql) as $row) {
-        echo "<tr><td>".$row[0]."</td><td>".$row[2]."</td><td>".$row[1]."</td></tr>";
+        echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td></tr>";
     }
     echo "</table>";
 ?>
