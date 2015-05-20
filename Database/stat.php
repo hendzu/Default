588a8acef89bad004cbe1bdeee9574d1027a62fe
dianx93 <dianx93@gmail.com>
2015-05-20 11:04:49 +0300

    Võtsin vigase ära

--- Database/stat.php
+++ Database/stat.php
@@ -35,16 +35,7 @@ group by kandidaadid.nimi;";
     echo "</table>";
     ?>
 <h3>Häälte jagunemine kogu riigis</h3>
-<?php
-    $sql ="Select kandidaadid.Nimi, count(*), parteid.Nimi From haaled h join kandidaadid k on 
-        h.kandidaat_id=k.id JOIN parteid p ON p.id=k.partei_id group by kandidaadid.id;";
-    echo "<table>";
-    echo "<tr><th>Kandidaat</th><th>Partei</th><th>Hääli</th></tr>";
-    foreach ($conn->query($sql) as $row) {
-        echo "<tr><td>".$row[0]."</td><td>".$row[2]."</td><td>".$row[1]."</td></tr>";
-    }
-    echo "</table>";
-?>
+
 
 <h3>Häälte jagunemine piirkondade lõikes</h3>
 <h3>Häälte jagunemine parteide lõikes</h3>
