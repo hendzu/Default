a104a13a6b65e1a3304fc2b2db3c4846a87bc968
dianx93 <dianx93@gmail.com>
2015-05-21 20:14:55 +0300

    #10 meta tag

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>Default Valimised</title>
                 <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1">
                 <link rel="stylesheet" type="text/css" href="Styles/mySite.css">
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
