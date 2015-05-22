9f761be3fa83bb9415ea0af53f495e9da9affca7
dianx93 <dianx93@gmail.com>
2015-05-22 19:32:10 +0300

    no vaatame mis siin töötab

--- Database/stat.php
+++ Database/stat.php
@@ -1,79 +1,83 @@
 <?php
 require("connect.php");
-    $sql ="Select parteid.Nimi,count(*) From kandidaadid join parteid on kandidaadid.partei_id=parteid.id
+    $sqlpiirkonnad="Select piirkonnad.Piirkond from piirkonnad;";
+    $sqlparteid="Select parteid.Nimi from parteid;";
+
+    $sql1 ="Select parteid.Nimi,count(*) From kandidaadid join parteid on kandidaadid.partei_id=parteid.id
 group by parteid.nimi;";
     echo "<div id=\"tabelid\">";
     echo "<h3>Valimistel osalevad parteid.</h3>";
     echo "<table>";
     echo "<tr><th>Partei</th><th>Kandidaatide arv</th></tr>";
-    foreach ($conn->query($sql) as $row) {
+    foreach ($conn->query($sql1) as $row) {
         echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td></tr>";
     }
     echo "</table>";
 	
 	
-    echo "<h3>Kandidaatide hulk piirkonna kaupa.</h3>";
-	$sql ="Select piirkonnad.Piirkond,count(*)
+    echo "<h3>Kandidaatide hulk piirkonnas.</h3>";
+	$sql2 ="Select piirkonnad.Piirkond,count(*)
 From kandidaadid join piirkonnad on kandidaadid.piirkond_id=piirkonnad.id
 group by piirkonnad.Piirkond;";
     echo "<table>";
     echo "<tr><th>Piirkond</th><th>Kandidaatide arv</th></tr>";
-    foreach ($conn->query($sql) as $row) {
+    foreach ($conn->query($sql2) as $row) {
         echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td></tr>";
     }
-	
     echo "</table>";
-    ?>
-<h3>Häälte jagunemine kogu riigis</h3>
-<?php
-    $sql ="Select kandidaadid.Nimi, parteid.Nimi, piirkonnad.Piirkond, count(*)
+    
+    echo '<h3>Hääli saanud kandidaadid</h3>';
+    //kaks dropdowni 'piirkond' ja 'partei'
+    echo 'Piirkond:';
+    echo '<select name="valik1[piirkond]">';
+    echo '<option value="koik">Kõik</option>';
+    foreach ($conn->query($sqlpiirkonnad) as $row) {
+        echo '<option value='.$row[0].'>'.$row[0].'</option>';
+    }
+    echo '</select>';
+    $piirkond1=$valik1['piirkond'];
+    echo 'Valitud '.$piirkond1;
+    $sql3 ="Select kandidaadid.Nimi, parteid.Nimi, piirkonnad.Piirkond, count(*)
     From kandidaadid join parteid on kandidaadid.partei_id=parteid.id 
     join piirkonnad on kandidaadid.piirkond_id=piirkonnad.id 
     join haaled on haaled.kandidaat_id=kandidaadid.id
     group by kandidaadid.nimi, parteid.Nimi, piirkonnad.Piirkond
     ORDER BY count(*) DESC;";
-    
     echo "<table>";
     echo "<tr><th>Kandidaat</th><th>Partei</th><th>Piirkond</th><th>Hääled</th></tr>";
-    foreach ($conn->query($sql) as $row) {
+    foreach ($conn->query($sql3) as $row) {
         echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td></tr>";
     }
     echo "</table>";
-?>
 
-<h3>Häälte jagunemine piirkondade lõikes</h3>
-<?php
-    $sql ="Select piirkonnad.Piirkond, count(*)
+    echo'<h3>Häälte jagunemine piirkondade lõikes</h3>';
+    $sql4 ="Select piirkonnad.Piirkond, count(*)
     From kandidaadid join parteid on kandidaadid.partei_id=parteid.id 
     join piirkonnad on kandidaadid.piirkond_id=piirkonnad.id 
     join haaled on haaled.kandidaat_id=kandidaadid.id
     group by piirkonnad.Piirkond
     ORDER BY count(*) DESC;";
-    
     echo "<table>";
     echo "<tr><th>Piirkond</th><th>Hääled</th></tr>";
-    foreach ($conn->query($sql) as $row) {
+    foreach ($conn->query($sql4) as $row) {
         echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td></tr>";
     }
     echo "</table>";
-?>
-<h3>Häälte jagunemine parteide lõikes</h3>
-<?php
-    $sql ="Select parteid.Nimi, count(*)
+    
+    echo'<h3>Häälte jagunemine parteide lõikes</h3>';
+    $sql5 ="Select parteid.Nimi, count(*)
     From kandidaadid join parteid on kandidaadid.partei_id=parteid.id 
     join haaled on haaled.kandidaat_id=kandidaadid.id
     group by parteid.Nimi
     ORDER BY count(*) DESC;";
-    
     echo "<table>";
     echo "<tr><th>Partei</th><th>Hääled</th></tr>";
-    foreach ($conn->query($sql) as $row) {
+    foreach ($conn->query($sql5) as $row) {
         echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td></tr>";
     }
     echo "</table>";
-?>
-<h3>Häälte jagunemine konkreetsete kandidaatide lõikes</h3>
-<?php
-echo "</div>";
+    
+    echo '<h3>Häälte jagunemine konkreetsete kandidaatide lõikes</h3>';
+    echo "</div>";
 require("disconnect.php");
 ?>
