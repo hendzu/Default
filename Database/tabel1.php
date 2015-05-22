581215ff1f27d05504df0196ab5285d8f450b36f
dianx93 <dianx93@gmail.com>
2015-05-22 23:42:21 +0300

    aaand it doesn't work

--- Database/tabel1.php
+++ Database/tabel1.php
@@ -6,33 +6,33 @@ $piirkond=$_POST['valitudpiirkond'];
 try{
     if(!$partei.equals('koik') && !$piirkond.equals('koik')){
         echo '1';
-    $sql ="Select kandidaadid.Nimi, parteid.Nimi, piirkonnad.Piirkond, count(*)
+    /*$sql ="Select kandidaadid.Nimi, parteid.Nimi, piirkonnad.Piirkond, count(*)
     From kandidaadid join parteid on kandidaadid.partei_id=parteid.id 
     join piirkonnad on kandidaadid.piirkond_id=piirkonnad.id 
     join haaled on haaled.kandidaat_id=kandidaadid.id
     where parteid.Nimi=".$partei." and piirkonnad.Piirkond=".$piirkond."
     group by kandidaadid.nimi, parteid.Nimi, piirkonnad.Piirkond
-    ORDER BY count(*) DESC;";}
+    ORDER BY count(*) DESC;";}*/
     else if($partei.equals('koik') && !$piirkond.equals('koik')){
         echo '2';
-    $sql ="Select kandidaadid.Nimi, parteid.Nimi, piirkonnad.Piirkond, count(*)
+    /*$sql ="Select kandidaadid.Nimi, parteid.Nimi, piirkonnad.Piirkond, count(*)
     From kandidaadid join parteid on kandidaadid.partei_id=parteid.id 
     join piirkonnad on kandidaadid.piirkond_id=piirkonnad.id 
     join haaled on haaled.kandidaat_id=kandidaadid.id
     where piirkonnad.Piirkond=".$piirkond."
     group by kandidaadid.nimi, parteid.Nimi, piirkonnad.Piirkond
-    ORDER BY count(*) DESC;";}
+    ORDER BY count(*) DESC;";}*/
     else if($piirkond.equals('koik') && !$partei.equals('koik')){
-        echo '3';
+        echo '3';/*
     $sql ="Select kandidaadid.Nimi, parteid.Nimi, piirkonnad.Piirkond, count(*)
     From kandidaadid join parteid on kandidaadid.partei_id=parteid.id 
     join piirkonnad on kandidaadid.piirkond_id=piirkonnad.id 
     join haaled on haaled.kandidaat_id=kandidaadid.id
     where parteid.Nimi=".$partei."
     group by kandidaadid.nimi, parteid.Nimi, piirkonnad.Piirkond
-    ORDER BY count(*) DESC;";}
+    ORDER BY count(*) DESC;";}*/
     else {
-        echo '4';
+        echo '4';/*
     $sql ="Select kandidaadid.Nimi, parteid.Nimi, piirkonnad.Piirkond, count(*)
     From kandidaadid join parteid on kandidaadid.partei_id=parteid.id 
     join piirkonnad on kandidaadid.piirkond_id=piirkonnad.id 
@@ -45,7 +45,7 @@ try{
     foreach ($conn->query($sql) as $row) {
         echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td></tr>";
     }
-    echo "</table>";
+    echo "</table>";*/
 }
 catch (Exception $e) {
     echo "Tabelit ei olnud võimalik kuvada.";
