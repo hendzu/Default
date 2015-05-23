d9d5b9086581d60360bb3343366d7ed81586cd1a
dianx93 <dianx93@gmail.com>
2015-05-23 20:29:04 +0300

    ainult 1 tabel vol2

--- Database/tabel2.php
+++ Database/tabel2.php
@@ -3,21 +3,13 @@ require("connect.php");
 $piirkond=$_POST['valitudpiirkond'];
 
 try{
-    if(piirkond==1){
-    $sql ="Select parteid.Nimi, piirkonnad.Piirkond, count(*)
-    From kandidaadid join parteid on kandidaadid.partei_id=parteid.id 
-    join piirkonnad on kandidaadid.piirkond_id=piirkonnad.id 
-    join haaled on haaled.kandidaat_id=kandidaadid.id
-    group by parteid.Nimi, piirkonnad.Piirkond
-    ORDER BY count(*) DESC;";}
-    else {
     $sql ="Select parteid.Nimi, piirkonnad.Piirkond, count(*)
     From kandidaadid join parteid on kandidaadid.partei_id=parteid.id 
     join piirkonnad on kandidaadid.piirkond_id=piirkonnad.id 
     join haaled on haaled.kandidaat_id=kandidaadid.id
     where piirkonnad.Piirkond='".$piirkond."'
     group by parteid.Nimi, piirkonnad.Piirkond
-    ORDER BY count(*) DESC;";}
+    ORDER BY count(*) DESC;";
     
     echo "<table>";
     echo "<tr><th>Partei</th><th>Piirkond</th><th>Hääled</th></tr>";
