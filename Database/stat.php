17b0f8f723809626962f4af0c9ce65010d296a49
dianx93 <dianx93@gmail.com>
2015-05-23 20:08:04 +0300

    no kui ei tööta siis ei tööta ja siis ei tulegi

--- Database/stat.php
+++ Database/stat.php
@@ -52,7 +52,6 @@ group by piirkonnad.Piirkond;";
     echo '<h3>Hääli saanud parteid</h3>';
     echo 'Piirkond:';
     echo '<select id="valitudpiirkond" name="valitudpiirkond">';
-    echo '<option value="koik">Kõik</option>';
     foreach ($conn->query($sqlpiirkonnad) as $row) {
         echo '<option value="'.$row[0].'">'.$row[0].'</option>';
     }
